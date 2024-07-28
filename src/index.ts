9692e0af49abafa9e2a4afc9e6291c34749e20c8
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-07-28 17:39:11 +1200

    add download and download gunzipping

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import Executor from "./libraries/Executor"
 import { satisfies } from "semver"
 import DBDestroySignal from "./libraries/AbortSignal"
 import { InternalServerOptions, ServerOptions } from '../types'
+import getBinaryURL from './libraries/Version'
+import MySQLVersions from './versions.json'
+import { downloadBinary } from './libraries/Downloader'
 
 const defaultOptions: InternalServerOptions = {
     dbName: 'dbdata',
-    logLevel: 'ERROR'
+    logLevel: 'LOG'
 }
 
 process.on('exit', () => {
@@ -25,7 +28,14 @@ export async function createDB(opts: ServerOptions = defaultOptions) {
     logger.log('Data:', options.version, os.platform(), os.release(), os.arch())
     const version = await executor.getMySQLVersion()
     if (version === null || (options.version && !satisfies(version, options.version))) {
-        throw 'Desired version of mysqld is not installed. Support for downloading MySQL is coming soon.'
+        try {
+            const binaryURL = getBinaryURL(MySQLVersions, options.version)
+            logger.log('Downloading binary from url:', binaryURL)
+            await downloadBinary(binaryURL, logger);
+        } catch (e) {
+            logger.error(e)
+            throw 'Downloading updated versions list is coming soon'
+        }
     } else {
         logger.log(version)
         return await executor.startMySQL(options)
--- src/libraries/Downloader.ts
+++ src/libraries/Downloader.ts
@@ -1,26 +1,86 @@
-import * as http from 'http';
+import * as https from 'https';
 import * as fs from 'fs';
+import * as fsPromises from 'fs/promises'
+import * as os from 'os';
+import { v4 as uuidv4 } from 'uuid';
+import Logger from './Logger';
+import { createGunzip } from 'zlib';
 
-export default function Download(url: string, filepath: string): Promise<undefined> {
+export function downloadVersions(): Promise<string> {
     return new Promise((resolve, reject) => {
-        const fileStream = fs.createWriteStream(filepath);
+        let json = "";
+
+        https.get("https://github.com/Sebastian-Webster/mysql-memory-server-nodejs/raw/main/versions.json", function(response) {
+            response
+            .on("data", append => json += append )
+            .on("error", e => {
+                reject(e)
+            } )
+            .on("end", ()=>{
+                resolve(json)
+            } );
+        });
+    })
+}
+
+export function downloadBinary(url: string, logger: Logger): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+        const dirpath = `${os.tmpdir()}/mysqlmsn/binaries`
+        logger.log('Binary path:', dirpath)
+        await fsPromises.mkdir(dirpath, {recursive: true})
+
+        const uuid = uuidv4()
+        const zipFilepath = `${dirpath}/${uuid}.tar.gz`
+        const extractedFilepath = `${dirpath}/${uuid}.tar`
+
+        const fileStream = fs.createWriteStream(zipFilepath);
 
         fileStream.on('open', () => {
-            const request = http.get(url, (response) => {
+            const request = https.get(url, (response) => {
                 response.pipe(fileStream)
             })
 
             request.on('error', (err) => {
-                fs.unlink(filepath, () => {
+                logger.error(err)
+                fileStream.end()
+                fs.unlink(zipFilepath, () => {
                     reject(err);
                 })
             })
         })
 
-        fileStream.on('finish', resolve)
+        fileStream.on('finish', () => {
+            logger.log('Extracting binary...')
+
+            const zipReadStream = fs.createReadStream(zipFilepath)
+            const extractedWriteStream = fs.createWriteStream(extractedFilepath)
+
+            zipReadStream.pipe(createGunzip()).pipe(extractedWriteStream)
+
+            zipReadStream.on('error', (err) => {
+                extractedWriteStream.end()
+                fsPromises.unlink(zipFilepath)
+                fsPromises.unlink(extractedFilepath)
+                reject(err)
+            })
+
+            extractedWriteStream.on('error', (err) => {
+                zipReadStream.close();
+                fsPromises.unlink(zipFilepath)
+                fsPromises.unlink(extractedFilepath)
+                reject(err)
+            })
+            
+            extractedWriteStream.on('finish', () => {
+                logger.log('Finished extracting binary')
+                resolve()
+            })
+        })
 
         fileStream.on('error', (err) => {
-            fs.unlink(filepath, () => {
+            logger.error(err)
+            fileStream.end()
+            fs.unlink(zipFilepath, () => {
                 reject(err)
             })
         })
