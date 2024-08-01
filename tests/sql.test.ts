a703a61284c0a9940372b50fdb04b0d01aad221b
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-08-01 19:09:00 +1200

    changed test

--- tests/sql.test.ts
+++ tests/sql.test.ts
@@ -5,25 +5,18 @@ import { MySQLDB } from '../types';
 
 jest.setTimeout(1_000_000_000);
 
-let db: MySQLDB;
-let connection: sql.Connection;
-
-beforeEach(async () => {
-    db = await createDB({loglevel: 'LOG', dbName: 'test'})
-    connection = await sql.createConnection({
+test('Runs with installed version (or downloads version if one is not available)', async () => {
+    const db = await createDB({loglevel: 'LOG', dbName: 'test'})
+    const connection = await sql.createConnection({
         host: '127.0.0.1',
         user: 'root',
         port: db.port
     })
-})
 
-afterEach(async () => {
-    await connection.end();
-    await db.stop();
-})
-
-test('Runs with installed version (or downloads version if one is not available)', async () => {
     const result = await connection.query('SELECT 1 + 1')
 
     expect(result[0][0]['1 + 1']).toBe(2)
+
+    await connection.end();
+    await db.stop();
 })
