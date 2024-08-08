f3a9c2de8ecac7d920b4390ceed20a42a20414d7
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-08-08 17:21:24 +1200

    upload db artifacts

--- tests/sql.test.ts
+++ tests/sql.test.ts
@@ -8,7 +8,7 @@ jest.setTimeout(900_000);
 let db: MySQLDB;
 
 beforeEach(async () => {
-    db = await createDB({username: 'root', logLevel: 'LOG', deleteDBAfterStopped: !!process.env.CI})
+    db = await createDB({username: 'root', logLevel: 'LOG', deleteDBAfterStopped: !!process.env.CI, dbPath: process.env.CI ? './dbs' : undefined})
 })
 
 afterEach(async () => {
