337f999704b842d937b420b1bf50312232e29493
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-08-08 17:42:32 +1200

    use nano id for folder name

--- tests/sql.test.ts
+++ tests/sql.test.ts
@@ -2,13 +2,14 @@ import {expect, test, jest, beforeEach, afterEach} from '@jest/globals'
 import { createDB } from '../src/index'
 import sql from 'mysql2/promise'
 import { MySQLDB } from '../types';
+import { nanoid } from 'nanoid';
 
 jest.setTimeout(900_000);
 
 let db: MySQLDB;
 
 beforeEach(async () => {
-    db = await createDB({username: 'root', logLevel: 'LOG', deleteDBAfterStopped: !!process.env.CI, dbPath: process.env.CI ? `${process.cwd()}/dbs/${process.env.JEST_WORKER_ID}` : undefined})
+    db = await createDB({username: 'root', logLevel: 'LOG', deleteDBAfterStopped: !!process.env.CI, dbPath: process.env.CI ? `${process.cwd()}/dbs/${nanoid(8)}` : undefined})
 })
 
 afterEach(async () => {
--- tests/versions.test.ts
+++ tests/versions.test.ts
@@ -2,6 +2,7 @@ import {expect, test, jest} from '@jest/globals'
 import { createDB } from '../src/index'
 import sql from 'mysql2/promise'
 import { coerce } from 'semver';
+import { nanoid } from 'nanoid';
 
 const versions = ['9.0.1', '8.4.2', '8.0.39', '8.1.0', '8.2.0', '8.3.0']
 
@@ -9,7 +10,7 @@ jest.setTimeout(900_000);
 
 for (const version of versions) {
     test(`running on version ${version}`, async () => {
-        const db = await createDB({version, dbName: 'testingdata', username: 'root', logLevel: 'LOG', deleteDBAfterStopped: !!process.env.CI, dbPath: process.env.CI ? `${process.cwd()}/dbs/${process.env.JEST_WORKER_ID}` : undefined})
+        const db = await createDB({version, dbName: 'testingdata', username: 'root', logLevel: 'LOG', deleteDBAfterStopped: !!process.env.CI, dbPath: process.env.CI ? `${process.cwd()}/dbs/${nanoid(8)}` : undefined})
         const connection = await sql.createConnection({
             host: '127.0.0.1',
             user: db.username,
