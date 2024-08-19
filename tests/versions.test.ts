a0e6c6144997362beb6cd35e963ee55ed4413dff
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-08-19 15:59:42 +1200

    add username tests

--- tests/versions.test.ts
+++ tests/versions.test.ts
@@ -6,32 +6,35 @@ import { randomUUID } from 'crypto';
 import { ServerOptions } from '../types';
 
 const versions = ['9.0.1', '8.4.2', '8.0.39', '8.1.0', '8.2.0', '8.3.0']
+const usernames = ['root', 'dbuser', 'admin']
 
 const dbPathPrefix = process.platform === 'win32' ? 'C:\\Users\\RUNNER~1\\dbs' : '/tmp/dbs'
 
 jest.setTimeout(500_000);
 
 for (const version of versions) {
-    test(`running on version ${version}`, async () => {
-        Error.stackTraceLimit = Infinity
-        const options: ServerOptions = {version, dbName: 'testingdata', username: 'root', logLevel: 'LOG', deleteDBAfterStopped: !process.env.useCIDBPath, ignoreUnsupportedSystemVersion: true}
-
-        if (process.env.useCIDBPath) {
-            options.dbPath = `${dbPathPrefix}/${randomUUID()}`
-        }
-
-        const db = await createDB(options)
-        const connection = await sql.createConnection({
-            host: '127.0.0.1',
-            user: db.username,
-            port: db.port
+    for (const username of usernames) {
+        test(`running on version ${version} with username ${username}`, async () => {
+            Error.stackTraceLimit = Infinity
+            const options: ServerOptions = {version, dbName: 'testingdata', username: username, logLevel: 'LOG', deleteDBAfterStopped: !process.env.useCIDBPath, ignoreUnsupportedSystemVersion: true}
+    
+            if (process.env.useCIDBPath) {
+                options.dbPath = `${dbPathPrefix}/${randomUUID()}`
+            }
+    
+            const db = await createDB(options)
+            const connection = await sql.createConnection({
+                host: '127.0.0.1',
+                user: db.username,
+                port: db.port
+            })
+    
+            const mySQLVersion = (await connection.query('SELECT VERSION()'))[0][0]["VERSION()"]
+    
+            await connection.end();
+            await db.stop();
+    
+            expect(coerce(mySQLVersion)?.version).toBe(version)
         })
-
-        const mySQLVersion = (await connection.query('SELECT VERSION()'))[0][0]["VERSION()"]
-
-        await connection.end();
-        await db.stop();
-
-        expect(coerce(mySQLVersion)?.version).toBe(version)
-    })
+    }
 }
