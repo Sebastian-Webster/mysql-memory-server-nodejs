e67a97e412089997ba01d280a42aa35db9c8da97
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-10-24 16:51:37 +1300

    add initSQLString option

--- types/index.ts
+++ types/index.ts
@@ -17,7 +17,8 @@ export type ServerOptions = {
     port?: number,
     xPort?: number,
     binaryDirectoryPath?: string,
-    downloadRetries?: number
+    downloadRetries?: number,
+    initSQLString?: string | undefined
 }
 
 export type InternalServerOptions = {
@@ -35,7 +36,8 @@ export type InternalServerOptions = {
     port: number,
     xPort: number,
     binaryDirectoryPath: string,
-    downloadRetries: number
+    downloadRetries: number,
+    initSQLString: string
 }
 
 export type ExecutorOptions = {
