f8d23127b5a60891c303aba7b668a32d5adfd4b6
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-11-16 15:28:07 +1300

    add extra line breaks in console output for readability

--- src/cli.ts
+++ src/cli.ts
@@ -4,7 +4,7 @@ import { createDB } from "./index";
 async function main() {
     console.log('Creating ephemeral MySQL database...')
     const db = await createDB();
-    console.log(`A MySQL databases has been successfully created with the following parameters.\nUsername: ${db.username} \nDatabase Name: ${db.dbName} \nPort: ${db.port} \nX Plugin Port: ${db.xPort} \nSocket: ${db.socket} \nX Plugin Socket: ${db.xSocket}`)
+    console.log(`A MySQL databases has been successfully created with the following parameters.\n\nUsername: ${db.username} \nDatabase Name: ${db.dbName} \nPort: ${db.port} \nX Plugin Port: ${db.xPort} \nSocket: ${db.socket} \nX Plugin Socket: ${db.xSocket}\n`)
     console.log(`If you want to use the MySQL CLI client to connect to the database, you can use either commands: \nmysql -u ${db.username} -P ${db.port} --protocol tcp \nOR\nmysql -u ${db.username} --socket ${db.socket}`)
 }
 
