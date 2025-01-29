912c55852a23b247358d6f9fda9e2a4e9e802012
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2025-01-29 22:20:18 +0400

    change versions

--- tests/versions.test.ts
+++ tests/versions.test.ts
@@ -19,7 +19,7 @@ jest.setTimeout(500_000);
 
 const arch = process.arch === 'x64' || (process.platform === 'win32' && process.arch === 'arm64') ? 'x64' : 'arm64';
 
-for (const version of ['5.7.44', '8.0.11', '9.x']) {
+for (const version of ['8.0.11', '8.0.15', '8.0.20', '8.0.25', '8.0.30', '9.x']) {
     try {
         getBinaryURL(version, arch)
     } catch (e) {
