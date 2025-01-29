fa0c461bf752a04b0aa6f3e7757e354da91626b7
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2025-01-29 18:55:16 +0400

    decrease versions temporarily

--- tests/versions.test.ts
+++ tests/versions.test.ts
@@ -19,7 +19,7 @@ jest.setTimeout(500_000);
 
 const arch = process.arch === 'x64' || (process.platform === 'win32' && process.arch === 'arm64') ? 'x64' : 'arm64';
 
-for (const version of DOWNLOADABLE_MYSQL_VERSIONS) {
+for (const version of ['5.7.20', '9.x']) {
     try {
         getBinaryURL(version, arch)
     } catch (e) {
