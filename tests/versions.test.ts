e49fb008f4411325ca5696dff181bb09a07d1eb8
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2025-01-30 22:54:44 +0400

    test 5.7.19

--- tests/versions.test.ts
+++ tests/versions.test.ts
@@ -19,7 +19,7 @@ jest.setTimeout(500_000);
 
 const arch = process.arch === 'x64' || (process.platform === 'win32' && process.arch === 'arm64') ? 'x64' : 'arm64';
 
-for (const version of DOWNLOADABLE_MYSQL_VERSIONS.filter(v => satisfies(v, '>=5.7.20 <=5.7.44'))) {
+for (const version of ['5.7.19']) {
     try {
         getBinaryURL(version, arch)
     } catch (e) {
