711bcbac99ee095c607b157f109f65fabbc973d5
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2026-08-17 11:49:31 +0400

    Move folder for CI diagnose instead of copying then deleting folder

--- tests/ci/DirMoveGitHubActions.js
+++ tests/ci/DirMoveGitHubActions.js
@@ -5,12 +5,34 @@ const os = require('os')
 const fs = require('fs')
 const fsPromises = require('fs/promises')
 
+async function moveFolder(startDir, endDir) {
+    const dir = await fsPromises.readdir(startDir, { withFileTypes: true })
+
+    for (const file of dir) {
+
+        if (file.isSocket()) continue;
+
+        if (file.isDirectory()) {
+            const start = `${file.parentPath}/${file.name}`
+            const dest = `${endDir}/${file.name}`
+            await fsPromises.mkdir(dest, { recursive: true })
+            await moveFiles(start, dest)
+        } else {
+            const start = `${file.parentPath}/${file.name}`
+            const dest = `${endDir}/${file.name}`
+            await fsPromises.rename(start, dest)
+        }
+    }
+
+    await fsPromises.rmdir(startDir)
+}
+
 async function main() {
     const originalPath = `${os.tmpdir()}/mysqlmsn`
     if (process.env.MOVE_MYSQLMSN_TO && fs.existsSync(originalPath) && originalPath !== process.env.MOVE_MYSQLMSN_TO) {
         console.log('Moving MySQLMSN directory to other path for GitHub Actions upload')
-        await fsPromises.cp(originalPath, process.env.MOVE_MYSQLMSN_TO, {recursive: true, force: true, filter: source => !source.includes('.sock')})
-        await fsPromises.rm(originalPath, {force: true, recursive: true, maxRetries: 50, retryDelay: 100})
+        await fsPromises.mkdir(process.env.MOVE_MYSQLMSN_TO, { recursive: true })
+        await moveFolder(originalPath, process.env.MOVE_MYSQLMSN_TO)
     } else {
         console.log('Skipping MySQLMSN directory move as conditions are not met.')
     }
